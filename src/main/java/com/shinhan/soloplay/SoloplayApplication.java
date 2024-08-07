f8215b8e926154d9165dea956b78d9325484a272
kimsh142536 <kimsh142536@gmail.com>
2024-08-07 14:00:40 +0900

    test

---
+++ src/main/java/com/shinhan/soloplay/SoloplayApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.soloplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shinhan/soloplay/SoloplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.soloplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
