87320368152adcbb50f9a1a76b1715f7fcf9638d
eundongg <remy08@naver.com>
2024-08-12 14:31:11 +0900

    [Fix]엔티티 수정

--- src/main/java/com/shinhan/soloplay/coupon/CouponHistoryEntity.java
+++ src/main/java/com/shinhan/soloplay/coupon/CouponHistoryEntity.java
@@ -4,10 +4,15 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+import com.shinhan.soloplay.user.UserEntity;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,4 +32,12 @@ public class CouponHistoryEntity {
 	private Integer couponHistoryId;
 	private int isUsed;
 	private LocalDateTime expirationDate;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "coupon_id",nullable = false)
+    private CouponEntity coupon;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="user_id",nullable = false)
+	private UserEntity user;
 }
--- src/main/java/com/shinhan/soloplay/point/PointEntity.java
+++ src/main/java/com/shinhan/soloplay/point/PointEntity.java
@@ -4,10 +4,15 @@ import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import com.shinhan.soloplay.user.UserEntity;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,6 +36,8 @@ public class PointEntity {
 	private LocalDateTime deadDate;
 	private int category;
 	
-	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="user_id", nullable = false)
+	private UserEntity user;
 	
 }
