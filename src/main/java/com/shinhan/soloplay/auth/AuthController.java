1d989d28b678621a9bb20bef752fc4e7801f703b
kimsh142536 <kimsh142536@gmail.com>
2024-08-14 14:47:42 +0900

    [Feature] Userlogin

--- src/main/java/com/shinhan/soloplay/auth/AuthController.java
+++ src/main/java/com/shinhan/soloplay/auth/AuthController.java
@@ -1,8 +1,39 @@
 package com.shinhan.soloplay.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class AuthController {
-	
+
+    @Autowired
+    AuthService authService; 
+
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public String login(@RequestParam("userId") String userId, 
+                        @RequestParam("userPassword") String userPassword, 
+                        HttpSession httpSession) {
+        try {
+            AuthDTO loginUser = (AuthDTO) authService.loadUserByUsername(userId);
+
+            if (passwordEncoder.matches(userPassword, loginUser.getUser().getUserPassword())) {
+                httpSession.setAttribute("loginUser", loginUser);
+                return "로그인 성공";
+            } else {
+                return "비밀번호가 일치하지 않습니다.";
+            }
+
+        } catch (UsernameNotFoundException e) {
+            return "유저를 찾을 수 없습니다.";
+        }
+    }
 }
--- src/main/java/com/shinhan/soloplay/config/SecurityConfig.java
+++ src/main/java/com/shinhan/soloplay/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -17,6 +18,13 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+	
+    @Bean
+    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
