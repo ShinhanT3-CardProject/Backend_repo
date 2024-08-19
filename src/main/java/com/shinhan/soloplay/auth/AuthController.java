e6083ae65300f1092ef9c600af673231687b6186
kimsh142536 <kimsh142536@gmail.com>
2024-08-19 16:25:03 +0900

    [Feat] Login & Registration

--- src/main/java/com/shinhan/soloplay/auth/AuthController.java
+++ src/main/java/com/shinhan/soloplay/auth/AuthController.java
@@ -1,5 +1,7 @@
 package com.shinhan.soloplay.auth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shinhan.soloplay.user.UserDTO;
@@ -26,39 +27,44 @@ public class AuthController {
     BCryptPasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestParam("userId") String userId, 
-                        @RequestParam("userPassword") String userPassword, 
-                        HttpSession httpSession) {
-    	try {
+    public ResponseEntity<?> login(@RequestBody Map<String, String> requestBody, HttpSession httpSession) {
+        String userId = requestBody.get("userId");
+        String userPassword = requestBody.get("userPassword");
+
+        try {
             AuthDTO loginUser = (AuthDTO) authService.loadUserByUsername(userId);
 
             if (passwordEncoder.matches(userPassword, loginUser.getUser().getUserPassword())) {
                 httpSession.setAttribute("loginUser", loginUser);
                 return ResponseEntity.ok().body("로그인 성공");
             } else {
-            	return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("비밀번호가 불일치");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("비밀번호가 불일치");
             }
 
         } catch (UsernameNotFoundException e) {
-        	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("없는 유저입니다.");
-        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("없는 유저입니다.");
+        } catch (Exception e) {
             // 기타 오류 응답
+        	e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류.");
         }
     }
+
     
     @PostMapping("/registration")
     public ResponseEntity<String> registration(@RequestBody UserDTO signUpUser) {
-    	System.out.println(signUpUser);
-    	System.out.println(signUpUser);
-        boolean isSignUpSuccessful = authService.signUp(signUpUser);
-        
-        if (isSignUpSuccessful) {
+        int signUpResult = authService.signUp(signUpUser);
+
+        if (signUpResult == 1) {
             // 회원가입 성공
             return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 성공적으로 완료되었습니다.");
+        } else if (signUpResult == -1) {
+            // 중복된 유저
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 존재하는 유저입니다.");
         } else {
             // 회원가입 실패
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("회원가입에 실패했습니다.");
         }
     }
+
 }
