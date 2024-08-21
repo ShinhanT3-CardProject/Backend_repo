35f4755ca03fcc8f9a1bcfdf8d74eec865eb24f6
ExProcurement <160109697+ExProcurement@users.noreply.github.com>
2024-08-21 17:01:10 +0900

    [Fix] JPQL 전환관련 서버시작 오류 임시 해결

--- src/main/java/com/shinhan/soloplay/theme/ThemeRepository1.java
+++ src/main/java/com/shinhan/soloplay/theme/ThemeRepository1.java
@@ -11,13 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface ThemeRepository1 extends JpaRepository<ThemeEntity, Long> {
 	
 	// 전체 테마 조회
-	@Query("SELECT new com.shinhan.soloplay.theme.ThemeSearchDTO1(t.themeId, t.themeName, t.themeIsPublic, mc.themeMainCategoryId, mc.themeMainCategoryName, mc.themeBackground) " + 
-				"FROM ThemeEntity t " +
-				"JOIN t.themeContents tc " +
-				"JOIN tc.subCategory sc " + 
-				"JOIN sc.mainCategory mc " +
-				"WHERE t.themeIsPublic = 1"
-			)
+	@Query("SELECT t from ThemeEntity t")
 	List<ThemeSearchDTO1> findAllTheme();
 	
 	// 전체 테마 조회 (카테고리별 필터링, 공개여부 참)
@@ -30,6 +24,12 @@ public interface ThemeRepository1 extends JpaRepository<ThemeEntity, Long> {
 	List<ThemeSearchDTO1> findAllThemeFilter(@Param("themeMainCategoryId") Long themeMainCategoryId);
 	
 	// 테마 상세조회
+	@Query("SELECT new com.shinhan.soloplay.theme.ThemeSearchDTO1(t.themeId, t.user, t.themeName, t.themeDescription, t.themeIsActivated, t.themeIsPublic, t.themeCreateDate, t.themeUpdateDate, mc.themeMainCategoryId, mc.themeMainCategoryName, mc.themeBackground, sc.themeSubCategoryName) " +
+			"FROM ThemeEntity t " +
+			"JOIN t.themeContents tc " +
+			"JOIN tc.subCategory sc " + 
+			"JOIN sc.mainCategory mc " +
+			"WHERE t.themeId=1")
 	ThemeSearchDTO1 findThemeDetail(Long themeId);
 	
     // 나의 테마 조회
@@ -42,16 +42,20 @@ public interface ThemeRepository1 extends JpaRepository<ThemeEntity, Long> {
 	List<ThemeSearchDTO1> findMyTheme(@Param("userId") String userId);
 	
 	// 나의 테마 상세조회
-	ThemeSearchDTO1 findMyThemeDetail(Long themeId);
+    @Query("SELECT new com.shinhan.soloplay.theme.ThemeSearchDTO1(t.themeId, t.user, t.themeName, t.themeDescription, t.themeIsActivated, t.themeIsPublic, t.themeCreateDate, t.themeUpdateDate, mc.themeMainCategoryId, mc.themeMainCategoryName, mc.themeBackground, sc.themeSubCategoryName) FROM ThemeEntity t JOIN t.themeContents tc JOIN tc.subCategory sc JOIN sc.mainCategory mc WHERE t.themeId = :themeId")
+    ThemeSearchDTO1 findMyThemeDetail(@Param("themeId") Long themeId);
 	
 	// 테마 수정 (나의 테마 상세조회에서 가능)
-	ThemeRegisterDTO1 updateTheme(Long themeId, ThemeRegisterDTO1 themeRegisterDTO1);
+//	@Query("SELECT * from ThemeEntity")
+//	ThemeRegisterDTO1 updateTheme(Long themeId, ThemeRegisterDTO1 themeRegisterDTO1);
 		
 	// 테마 삭제 (나의 테마 상세조회에서 가능)
-	void deleteTheme(Long themeId);
+//	@Query("SELECT * from ThemeEntity")
+//	void deleteTheme(Long themeId);
 		
 	// 테마 등록
-	ThemeRegisterDTO1 insertTheme(ThemeRegisterDTO1 themeRegisterDTO1);
+//	@Query("SELECT * from ThemeEntity")
+//	ThemeRegisterDTO1 insertTheme(ThemeRegisterDTO1 themeRegisterDTO1);
 
 	// 테마 불러오기 (테마 등록, 테마 수정에서 가능)
 	@Query("SELECT new com.shinhan.soloplay.theme.ThemeSearchDTO1(t.themeId, t.user, t.themeName, t.themeDescription, t.themeIsActivated, t.themeIsPublic, t.themeCreateDate, t.themeUpdateDate, mc.themeMainCategoryId, mc.themeMainCategoryName, mc.themeBackground, sc.themeSubCategoryName) " +
