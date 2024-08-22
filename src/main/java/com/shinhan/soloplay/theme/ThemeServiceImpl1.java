7bd99dff799cbde80622eb153a367e780f2ef70d
ExProcurement <160109697+ExProcurement@users.noreply.github.com>
2024-08-22 16:45:08 +0900

    [Feat] themeSearch, themeDetail 조회 DB연동 및 FrontEnd 연결 진행

--- src/main/java/com/shinhan/soloplay/theme/ThemeServiceImpl1.java
+++ src/main/java/com/shinhan/soloplay/theme/ThemeServiceImpl1.java
@@ -1,34 +1,81 @@
 package com.shinhan.soloplay.theme;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ThemeServiceImpl1 implements ThemeService1 {
 	
-	@Autowired
-	ThemeRepository1 themeRepository1;
+	private final ThemeRepository1 themeRepository1;
 
-	// 전체 테마 조회
+	// 전체 테마 조회 (공개여부 참) - 완료
 	@Override
-	public List<ThemeEntity> findAllTheme() {
-		return themeRepository1.findAll();
-	}
+	public Map<Long, Map<String, String>> findAllTheme() {
+		Map<Long, Map<String, String>> result = new HashMap<>();
+		
+		List<ThemeEntity> trueThemes = themeRepository1.findByThemeIsPublicTrue();
+		Map<Long, ThemeContentEntity> contents = new HashMap<>();
+		trueThemes.stream().forEach(theme->{
+			theme.getThemeContents().stream().forEach(content->{
+				contents.put(content.getThemeContentId(), content);
+			});
+			
+			contents.keySet().stream().forEach(contentId->{
+				Map<String, String> nameAndBack = new HashMap<>();
+				nameAndBack.put("themeName", contents.get(contentId).getTheme().getThemeName());
+				nameAndBack.put("themeBackground", contents.get(contentId).getSubCategory().getMainCategory().getThemeBackground());
+		
+				result.put(contents.get(contentId).getTheme().getThemeId(), nameAndBack);
+			});
+		});
+		
+		return result;
+  }
 	
-	// 전체 테마 조회 (카테고리별 필터링, 공개여부 참)
+	// 전체 테마 조회 (카테고리별 필터링)
 	@Override
 	public List<ThemeSearchDTO1> findAllThemeFilter(Long themeMainCategoryId) {
 		return themeRepository1.findAllThemeFilter(themeMainCategoryId);
 	}
     
-	// 테마 상세 조회
+	// 테마 상세 조회 - 블러오기까지는 완료, 복수의 테마를 담아오려면 조치 필요
     @Override
-    public ThemeSearchDTO1 findThemeDetail(Long themeId) {
-    	return themeRepository1.findMyThemeDetail(themeId);
-//    							.orElseThrow(() -> new IllegalArgumentException("유효하지 않은 theme ID"));
+    public Map<String ,?> findThemeDetail(Long themeId) {
+    	Map<String, Object> result = new HashMap<>(); //맨 마지막에 리턴할 값을 담아주는 용도
+    	Map<Long, ThemeContentEntity> contents = new HashMap<>();
+    	
+    	ThemeEntity themeEntity = themeRepository1.findByThemeId(themeId);
+    	result.put("themeName", themeEntity.getThemeName());
+    	result.put("themeDescription", themeEntity.getThemeDescription());
+    	
+    	themeEntity.getThemeContents().stream().forEach(content -> {
+    		contents.put(content.getThemeContentId(), content);
+    	});
+    	
+    	List<String> subCategories = new ArrayList<>();
+    	for(Long contentId:contents.keySet()) {
+    		result.put("themeMainCategoryName", contents.get(contentId).getSubCategory().getMainCategory().getThemeMainCategoryName());
+    		result.put("themeBackground", contents.get(contentId).getSubCategory().getMainCategory().getThemeBackground());
+    		subCategories.add(contents.get(contentId).getSubCategory().getThemeSubCategoryName());
+    	}
+    	result.put("themeSunCategoryName", subCategories);
+    	
+    	return result;
     }
+//    	contents.keySet().stream().forEach(contentId -> {
+//    		
+//    		result.put("themeMainCategoryName", contents.get(contentId).getSubCategory().getMainCategory().getThemeMainCategoryName());
+//    		result.put("themeBackground", contents.get(contentId).getSubCategory().getMainCategory().getThemeBackground());
+//    		result.put("themeSunCategoryName", contents.get(contentId).getSubCategory().getThemeSubCategoryName());
+//    		
+//    	});
 	
 	// 나의 테마 조회
 	@Override
@@ -39,7 +86,7 @@ public class ThemeServiceImpl1 implements ThemeService1 {
 	// 나의 테마 상세조회
     @Override
     public ThemeSearchDTO1 findMyThemeDetail(Long themeId) {
-    	return findThemeDetail(themeId);
+    	return findMyThemeDetail(themeId);
     }
     
 	// 테마 수정 (나의 테마 상세조회에서 가능)
